69314be5a7761e5074d93c1ba635a85af32727d0
Daven <davensteen@gmail.com>
2022-09-26 11:22:00 -0500

    capture input data

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const searchBtn = document.querySelector("#searchBtn");
+var cityEl = document.querySelector("#location")
+
+//objectify data based on user criteria
+var getUserInput = function(event){
+var userCity = cityEl.value;
+console.log(userCity);
+var 
+}
+
+// fetch data based on the user's inputs
+var getSearchData = function(){
+
+}
+
+searchBtn.addEventListener("click", getUserInput);
